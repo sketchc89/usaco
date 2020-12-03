9a161fdfe44da1fd41b19f6705deb4568fa5ba86
Chris Sketch <sketchc89@gmail.com>
2020-12-03 00:35:37 -0500

    base cpp file

---
+++ ch1/greedy/sample.cpp
@@ -0,0 +1,16 @@
+/*
+ID: sketchc1
+LANG: C++14
+TASK: milk
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    // i/o
+    ifstream fin("milk.in");
+    ofstream fout("milk.out");
+
+    return EXIT_SUCCESS;
+}
