db5af7ec94569032d1c09b0f74ec6d19bcf13a17
Chris Sketch <sketchc89@gmail.com>
2020-12-22 01:27:11 -0500

    add fence template

---
+++ ch3/s3/fence.cpp
@@ -0,0 +1,15 @@
+/*
+ID: sketchc1
+LANG: C++14
+TASK: fence
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // i/o
+    ifstream fin("fence.in");
+    ofstream fout("fence.out");
+
+    return EXIT_SUCCESS;
+}
