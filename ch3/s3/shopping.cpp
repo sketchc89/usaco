acb07bd1199c05e06113120e0f337f71be338e05
Chris Sketch <sketchc89@gmail.com>
2020-12-22 20:49:23 -0500

    comments

--- ch3/s3/shopping.cpp
+++ ch3/s3/shopping.cpp
@@ -10,14 +10,6 @@ constexpr static int INF = 1'000'000'000;
 constexpr static int SZ = 6;
 
 struct Offer {
-    Offer()
-            : prod{}
-            , price{} {}
-
-    Offer(unordered_map<int, int> _prod, int _price)
-            : prod{_prod}
-            , price{_price} {}
-
     unordered_map<int, int> prod;
     int price;
 };
@@ -27,10 +19,10 @@ int main() {
     ifstream fin("shopping.in");
     ofstream fout("shopping.out");
 
+    /* Record all special offers */
     int numOffers;
     fin >> numOffers;
     vector<Offer> offers(numOffers);
-    // cout << "offers: " << offers.size() << '\n';
     for (int i = 0; i < offers.size(); ++i) {
         int numProducts;
         fin >> numProducts;
@@ -42,42 +34,30 @@ int main() {
         fin >> offers[i].price;
     }
 
+    /* Record every normal price as an offer of one product */
     int numProducts;
     fin >> numProducts;
-    unordered_map<int, int> needs;
-    unordered_map<char, int> sku;
+    unordered_map<int, int> needs;  // what customer needs to buy
+    unordered_map<char, int> sku;   // mapping between dimension of dp and product number
     offers.resize(numProducts + numOffers);
-    // cout << "offers: " << offers.size() << '\n';
     for (int i = 0; i < numProducts; ++i) {
         int productNumber, productQuantity, productPrice;
         fin >> productNumber >> productQuantity >> productPrice;
+
         sku['a' + i] = productNumber;
         needs[productNumber] = productQuantity;
-        // cout << productNumber << '\t' << productQuantity << '\t' << productPrice << '\n';
         offers[i + numOffers].prod[productNumber] = 1;
         offers[i + numOffers].price = productPrice;
     }
 
-    // for (int i = 0; i < offers.size(); ++i) {
-    //     cout << "Offer# " << i << '\t' << "$" << offers[i].price << '\t';
-    //     for (auto&& kv : offers[i].prod) {
-    //         cout << "prod: " << kv.first << " quantity; " << kv.second << '\t';
-    //     }
-    //     cout << '\n';
-    // }
-
-    // for (auto&& kv : sku) {
-    //     cout << static_cast<char>(kv.first) << ':' << kv.second << '\n';
-    // }
-
-
+    /* represent the minimum price for a items w/ product number sku['a'] b items w// product number sku['b'], ...*/
     auto dp = array<array<array<array<array<int, SZ>, SZ>, SZ>, SZ>, SZ>{};
     for (int a = 0; a < SZ; ++a) {
         for (int b = 0; b < SZ; ++b) {
             for (int c = 0; c < SZ; ++c) {
                 for (int d = 0; d < SZ; ++d) {
                     for (int e = 0; e < SZ; ++e) {
-                        dp[a][b][c][d][e] = INF;
+                        dp[a][b][c][d][e] = INF;  // set to high value
                     }
                 }
             }
@@ -85,21 +65,23 @@ int main() {
     }
     dp[0][0][0][0][0] = 0;
 
+    // upper bound inclusive for 5d loop
     int qtyA = sku.count('a') ? needs[sku['a']] : 0;
     int qtyB = sku.count('b') ? needs[sku['b']] : 0;
     int qtyC = sku.count('c') ? needs[sku['c']] : 0;
     int qtyD = sku.count('d') ? needs[sku['d']] : 0;
     int qtyE = sku.count('e') ? needs[sku['e']] : 0;
 
-    // cout << "Quantity: " << qtyA << ' ' << qtyB << ' ' << qtyC << ' ' << qtyD << ' ' << qtyE << '\n';
+    // note loop is inclusive of upper bound
     for (int a = 0; a <= qtyA; ++a) {
         for (int b = 0; b <= qtyB; ++b) {
             for (int c = 0; c <= qtyC; ++c) {
                 for (int d = 0; d <= qtyD; ++d) {
                     for (int e = 0; e <= qtyE; ++e) {
                         for (int i = 0; i < offers.size(); ++i) {
-                            // cout << "Offer: " << i << '\n';
                             int prevA, prevB, prevC, prevD, prevE;
+                            // check if product number exists
+                            // umap [] will create key with value 0 otherwise
                             if (offers[i].prod.count(sku['a'])) {
                                 prevA = a - offers[i].prod[sku['a']];
                             } else {
@@ -125,17 +107,14 @@ int main() {
                             } else {
                                 prevE = e;
                             }
-                            // cout << "Previous " << prevA << ',' << prevB << ',' << prevC << ',' << prevD << ',' << prevE <<
-                            // '\n';
+
+                            // check that enough products bought for offer
                             if (prevA < 0 || prevB < 0 || prevC < 0 || prevD < 0 || prevE < 0) {
                                 continue;
                             }
+                            // update if better than previous value
                             if (dp[prevA][prevB][prevC][prevD][prevE] != INF) {
-                                // cout << "Found a price for " << a << ',' << b << ',' << c << ',' << d << ',' << e << '\t';
                                 dp[a][b][c][d][e] = min(dp[a][b][c][d][e], dp[prevA][prevB][prevC][prevD][prevE] + offers[i].price);
-                                // cout << "previously " << setw(3) << dp[prevA][prevB][prevC][prevD][prevE] << '\t';
-                                // cout << "price " << setw(3) << offers[i].price << '\t';
-                                // cout << "now " << setw(3) << dp[a][b][c][d][e] << '\n';
                             }
                         }
                     }
@@ -143,9 +122,8 @@ int main() {
             }
         }
     }
+
     fout << dp[qtyA][qtyB][qtyC][qtyD][qtyE] << '\n';
 
-
-    // int bestRes = minPrice(0, needs, offers);
     return EXIT_SUCCESS;
 }
