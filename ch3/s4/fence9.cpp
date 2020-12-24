0b6d297c882286727123720bba0ad26e47084187
Chris Sketch <sketchc89@gmail.com>
2020-12-24 00:48:58 -0500

    use scan algorithm

--- ch3/s4/fence9.cpp
+++ ch3/s4/fence9.cpp
@@ -6,105 +6,57 @@ TASK: fence9
 #include <bits/stdc++.h>
 using namespace std;
 
-// true if positive, false if negative
-template <typename T>
-T signCrossProduct(const pair<T, T>& a, const pair<T, T>& b, const pair<T, T>& p) {
-    // cross product ab x ap where z is 0
-    T left = (b.first - a.first) * (p.second - a.second);
-    T right = (b.second - a.second) * (p.first - a.first);
-    if (left == right) {
-        return 0;
-    } else if (left > right) {
-        return 1;
-    } else {
-        return -1;
-    }
-}
-
 int main() {
     // i/o
     ifstream fin("fence9.in");
     ofstream fout("fence9.out");
 
-    int n, m, p;
+    double n, m, p;
     fin >> n >> m >> p;
-    auto A = make_pair(0, 0), B = make_pair(n, m), C = make_pair(p, 0);
-    pair<double, double> midpoint = make_pair((n + p) / 3.0, m / 3.0);
-    pair<double, double> a = A, b = B, c = C;
-    int signAB = signCrossProduct(a, b, midpoint);
-    int signAC = signCrossProduct(a, c, midpoint);
-    int signBC = signCrossProduct(b, c, midpoint);
-
-    // cout << signAB << '\t' << signAC << '\t' << signBC << '\n';
-
-    int64_t res = 0;
-    int prevY = 1;
-    bool goingUp = true;
-    for (int x = 1; x < max(n, p); ++x) {
-        if (x == n + 1) {
-            // cout << "Past the x position of the top of the triangle\n";
-            goingUp = false;  // going down from outside to in
-            prevY = m;        // set outside the triangle
-            // cout << "Previous y is now " << prevY << '\n';
-        }
-        int y = prevY;
-        while (true) {
-            auto pt = make_pair(x, y);
-            auto crossABP = signCrossProduct(A, B, pt);
-            auto crossACP = signCrossProduct(A, C, pt);
-            auto crossBCP = signCrossProduct(B, C, pt);
-            // cout << crossABP << '\t' << crossACP << '\t' << crossBCP << '\n';
-            if (goingUp) {
-                if (signAB != crossABP || signAC != crossACP || signBC != crossBCP) {
-                    cout << "Outside at " << x << ',' << y << "\tLast inside at " << y - 1 << "\t";
-                    res += y - 1;
-                    prevY = max(prevY, y - 1);  // last inside the triangle. start inside again
-                    break;
-                } else {
-                    cout << "Inside at " << x << ',' << y << '\t';
-                }
-                ++y;
-            } else {
-                if (signAB == crossABP && signAC == crossACP && signBC == crossBCP) {
-                    cout << "Inside the triangle breaking. Last outside at " << y + 1 << "\t";
-                    res += y;
-                    prevY = min(prevY, y + 1);  // last outside the triangle. start outside again
-                    break;
-                } else if (y == 0) {
-                    break;
-                } else {
-                    cout << "Outside at " << x << ',' << y << '\t';
-                }
-                --y;
-            }
-        }
-
-        cout << "Counted " << res << " lattice points at x = " << x << '\n';
+    if (m == 0) {  // not a triangle with an area special case
+        fout << 0 << '\n';
+        return EXIT_SUCCESS;
     }
-    cout << "Now removing\n";
-    if (p < n) {
-        prevY = 1;
-        for (int x = p + 1; x < max(n, p); ++x) {
-            int y = prevY;
-            while (true) {
-                auto pt = make_pair(x, y);
-                auto crossABP = signCrossProduct(A, B, pt);
-                auto crossACP = signCrossProduct(A, C, pt);
-                auto crossBCP = signCrossProduct(B, C, pt);
-                if (signAB == crossABP && signAC == crossACP && signBC == crossBCP) {
-                    cout << "Inside the triangle breaking. Last outside at " << y - 1 << "\t";
-                    res -= y - 1;
-                    prevY = max(prevY, y - 1);  // last outside the triangle. start outside again
-                    break;
-                } else if (y >= m) {
-                    break;
-                } else {
-                    cout << "Outside at " << x << ',' << y << '\t';
-                }
-                ++y;
-            }
-            cout << "Counted " << res << " lattice points at x = " << x << '\n';
+    //_
+    //| \_
+    //|   \// little prince snake eating elephant
+    //|   /
+    //|  /
+    //| /
+    //|/
+    //         _//
+    //       _/ /
+    //     _/  /
+    //   _/   /
+    // _/    /
+    ///     /
+    //|    /
+    //|   /
+    //|  /
+    //| /
+    //|/
+    // flip axis so that AC is the y axis; (m,n); (0, p)
+    double slopeAB = static_cast<double>(n) / m;
+    double slopeCB = static_cast<double>(n - p) / m;
+    int64_t res = 0;
+    for (double x = 1.0; x < m; ++x) {
+        double yCB = x * slopeCB + p;
+        double yAB = x * slopeAB;
+
+        /* set y of cb to the point on the line if its very close otherwise the point outside the triangle */
+        if (yCB - floor(yCB) > FLT_EPSILON) {
+            yCB = floor(yCB) + 1;
+        } else {
+            yCB = floor(yCB);
         }
+        /* set y of ab to the point on the line if its very close otherwise the point outside the triangle */
+        if (ceil(yAB) - yAB > FLT_EPSILON) {
+            yAB = ceil(yAB) - 1;
+        } else {
+            yAB = ceil(yAB);
+        }
+
+        res += max(0, static_cast<int>(yCB - yAB - 1));
     }
 
     fout << res << '\n';
