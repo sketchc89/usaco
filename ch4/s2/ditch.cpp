1cbe738ad50003c12d530c18e53a7f9deba99478
Chris Sketch <sketchc89@gmail.com>
2020-12-29 12:50:02 -0500

    tle, need to use better path finding algo

--- ch4/s2/ditch.cpp
+++ ch4/s2/ditch.cpp
@@ -19,9 +19,19 @@ int main() {
     // maybe sorting edges by weight would be a bit faster
     unordered_map<int, unordered_map<int, int>> graph;
     int src, dst, weight;
-    while (fin >> src >> dst >> weight) {
-        graph[src][dst] = weight;
+    for (int i = 0; i < numEdges; ++i) {
+        fin >> src >> dst >> weight;
+        if (src != dst) {
+            graph[src][dst] = weight;
+        }
     }
+    // for (auto&& kv : graph) {
+    //     cout << kv.first << ':';
+    //     for (auto&& toWeight : kv.second) {
+    //         cout << toWeight.first << ',' << toWeight.second << '\t';
+    //     }
+    //     cout << '\n';
+    // }
 
     // keep adding flow until there is no change from one step to the next
     bool progress = true;
@@ -36,35 +46,65 @@ int main() {
         progress = false;
         /* there's probably a better data structure but I had already done iterative dfs
          * before realizing i needed to move around the path as a vector */
-        stack<pair<pair<int, int>, vector<int>>> s;
+        stack<pair<pair<int, int>, pair<vector<int>, unordered_set<int>>>> s;
+        unordered_set<int> inPath;
 
-        s.push({{flow, node}, path});
+        s.push({{flow, node}, {path, inPath}});
         while (!s.empty()) {
             flow = s.top().first.first;
             node = s.top().first.second;
-            path = s.top().second;
+            path = s.top().second.first;
+            inPath = s.top().second.second;
+            // cout << node << '\t' << flow << '\t';
+            // for (auto num : path) {
+            //     cout << num << ',';
+            // }
+            // cout << '\n';
             s.pop();
 
+
             // if we reach the destination and there is flow then report progress has been made
-            if (node == dst) {
-                progress = flow > 0;
+            if (node == dst && flow > 0) {
+                // cout << "Reached destination with " << flow << '\n';
+                progress = true;
                 break;
+            } else if (node == dst) {
+                // cout << "Reached destination with no flow\n";
+                continue;
             }
+
             // go down everry path that still has capacity
             for (auto& kv : graph[node]) {
+                // cout << "Edge " << node << "-->" << kv.first << '\t';
+                if (inPath.count(kv.first)) {
+                    // cout << "Already seen " << kv.first << '\n';
+                    continue;
+                    //     cout << "Haven't seen " << kv.first << '\t';
+                }
+
+                // cout << "previous flow " << flow << '\t' << " new edge flow " << kv.second << '\n';
                 int f = min(flow, kv.second);
                 if (f > 0) {
+                    // cout << " flow " << f << " greater than 0\n";
+                    inPath.insert(kv.first);
                     path.push_back(kv.first);
-                    s.push({{f, kv.first}, path});
+                    s.push({{f, kv.first}, {path, inPath}});
                     path.pop_back();
+                    inPath.erase(kv.first);
+                    // } else {
+                    //     cout << " no flow\n";
                 }
             }
         }
+
         if (progress) {
             totalFlow += flow;
+            // cout << "Progress, increasing by " << flow << " to " << totalFlow << '\n';
             // reduce capacity of every edge in the path found
             for (int i = 0; i < path.size() - 1; ++i) {
-                graph[path[i]][path[i + 1]] -= flow;
+                // cout << "Decreasing " << path[i] << "-->" << path[i + 1] << '\n';
+                graph[path[i]][path[i + 1]] -= flow;  // reduce flow from path
+                graph[path[i + 1]][path[i]] += flow;  // add residual flow in opposite direction of path
             }
         }
     }
