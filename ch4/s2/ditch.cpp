0cc42d7ed6e49ec2e1efd7af1dd26cd60fdca867
Chris Sketch <sketchc89@gmail.com>
2021-01-02 21:21:34 -0500

    failed attempt to fix dijkstra maxflow

--- ch4/s2/ditch.cpp
+++ ch4/s2/ditch.cpp
@@ -8,104 +8,111 @@ using namespace std;
 
 constexpr static int INF = 1'000'000'000;
 
+ofstream fout("ditch.out");
+
+int maxFlow(int src, int dst, unordered_map<int, unordered_map<int, pair<int, int>>>& graph, vector<int>& path, const int numNodes) {
+    vector<int> width(numNodes, 0);
+    vector<int> visited(numNodes, 0), parent(numNodes, -1);
+    width[0] = INF;
+    priority_queue<pair<int, int>> pq;
+
+    fout << '\n' << string(30, '-') << "\nFinding max flow path\n" << string(30, '-') << '\n';
+    pq.emplace(make_pair(width[0], src));
+    while (!pq.empty()) {
+        auto node = pq.top().second;
+        fout << "Node: " << node << "\t";
+        pq.pop();
+        if (visited[node]) {
+            fout << '\n';
+            continue;
+        } else {
+            visited[node] = true;
+        }
+        for (auto&& kv : graph[node]) {
+            int maxW = min(width[node], kv.second.first);
+            fout << "max width from " << node << " to " << kv.first << " is max of " << width[node] << " and " << kv.second.first << '\n';
+            if (maxW > width[kv.first]) {
+                parent[kv.first] = node;
+                width[kv.first] = maxW;
+                pq.emplace(make_pair(maxW, kv.first));
+            }
+        }
+    }
+
+    int x = dst;
+    path.push_back(x);
+    while (parent[x] != -1) {
+        fout << "Parent of " << x << " is " << parent[x] << '\t';
+        path.push_back(parent[x]);
+        x = parent[x];
+    }
+    reverse(begin(path), end(path));
+    // for (auto num : path) {
+    //     fout << num << ',';
+    // }
+    // fout << '\n';
+    return width[dst];
+}
+
 int main() {
     // i/o
     ifstream fin("ditch.in");
-    ofstream fout("ditch.out");
 
     // read input into graph
     int numEdges, numNodes;
     fin >> numEdges >> numNodes;
     // maybe sorting edges by weight would be a bit faster
-    unordered_map<int, unordered_map<int, int>> graph;
+    unordered_map<int, unordered_map<int, pair<int, int>>> graph;
     int src, dst, weight;
     for (int i = 0; i < numEdges; ++i) {
         fin >> src >> dst >> weight;
+        --src;
+        --dst;
         if (src != dst) {
-            graph[src][dst] = weight;
+            graph[src][dst].first = weight;
+            graph[src][dst].second = weight;
+            graph[dst][src].first = 0;
+            graph[dst][src].second = weight;
         }
     }
     // for (auto&& kv : graph) {
-    //     cout << kv.first << ':';
+    //     fout << kv.first << ':';
     //     for (auto&& toWeight : kv.second) {
-    //         cout << toWeight.first << ',' << toWeight.second << '\t';
+    //         fout << toWeight.first << ',' << toWeight.second << '\t';
     //     }
-    //     cout << '\n';
+    //     fout << '\n';
     // }
 
     // keep adding flow until there is no change from one step to the next
     bool progress = true;
     int totalFlow = 0;
-    while (progress) {
-        src = 1;  // 1 based b/c of input and we're using hash maps
-        dst = numNodes;
-        int flow = INF;
+    vector<int> path;
+    while (true) {
+        src = 0;
+        dst = numNodes - 1;
         int node = src;
-        vector<int> path(1, node);
 
-        progress = false;
-        /* there's probably a better data structure but I had already done iterative dfs
-         * before realizing i needed to move around the path as a vector */
-        stack<pair<pair<int, int>, pair<vector<int>, unordered_set<int>>>> s;
-        unordered_set<int> inPath;
-
-        s.push({{flow, node}, {path, inPath}});
-        while (!s.empty()) {
-            flow = s.top().first.first;
-            node = s.top().first.second;
-            path = s.top().second.first;
-            inPath = s.top().second.second;
-            // cout << node << '\t' << flow << '\t';
-            // for (auto num : path) {
-            //     cout << num << ',';
-            // }
-            // cout << '\n';
-            s.pop();
-
-
-            // if we reach the destination and there is flow then report progress has been made
-            if (node == dst && flow > 0) {
-                // cout << "Reached destination with " << flow << '\n';
-                progress = true;
-                break;
-            } else if (node == dst) {
-                // cout << "Reached destination with no flow\n";
-                continue;
-            }
-
-            // go down everry path that still has capacity
-            for (auto& kv : graph[node]) {
-                // cout << "Edge " << node << "-->" << kv.first << '\t';
-                if (inPath.count(kv.first)) {
-                    // cout << "Already seen " << kv.first << '\n';
-                    continue;
-                    //     cout << "Haven't seen " << kv.first << '\t';
-                }
-
-                // cout << "previous flow " << flow << '\t' << " new edge flow " << kv.second << '\n';
-                int f = min(flow, kv.second);
-                if (f > 0) {
-                    // cout << " flow " << f << " greater than 0\n";
-                    inPath.insert(kv.first);
-                    path.push_back(kv.first);
-                    s.push({{f, kv.first}, {path, inPath}});
-                    path.pop_back();
-                    inPath.erase(kv.first);
-                    // } else {
-                    //     cout << " no flow\n";
-                }
-            }
+        path.clear();
+        int flow = maxFlow(src, dst, graph, path, numNodes);
+        if (flow == 0) {
+            break;
         }
 
-        if (progress) {
-            totalFlow += flow;
-            // cout << "Progress, increasing by " << flow << " to " << totalFlow << '\n';
-            // reduce capacity of every edge in the path found
-            for (int i = 0; i < path.size() - 1; ++i) {
-                // cout << "Decreasing " << path[i] << "-->" << path[i + 1] << '\n';
-                graph[path[i]][path[i + 1]] -= flow;  // reduce flow from path
-                graph[path[i + 1]][path[i]] += flow;  // add residual flow in opposite direction of path
+        totalFlow += flow;
+        fout << "Progress, increasing by " << flow << " to " << totalFlow << '\n';
+        // reduce capacity of every edge in the path found
+        for (int i = 0; i < path.size() - 1; ++i) {
+            auto a = path[i];
+            auto b = path[i + 1];
+            fout << "Decreasing " << a << "-->" << b << '\t';
+            if (graph[a][b].first - flow < -graph[a][b].second) {
+                flow = graph[a][b].second - graph[a][b].first;
             }
+            if (graph[b][a].first + flow > graph[b][a].second) {
+                flow = graph[b][a].first - graph[b][a].second;
+            }
+            graph[a][b].first = graph[a][b].first - flow;  // reduce flow from path
+            graph[b][a].first = graph[b][a].first + flow;  // add residual flow in opposite direction of path
         }
     }
 
