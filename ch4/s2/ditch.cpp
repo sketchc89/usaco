cd1e54ca8259a829b09b30576c720c8ba99b334c
Chris Sketch <sketchc89@gmail.com>
2021-01-04 12:20:42 -0500

    add comments to help explain program

--- ch4/s2/ditch.cpp
+++ ch4/s2/ditch.cpp
@@ -17,13 +17,17 @@ struct Edge {
     }
 };
 
+// use bfs to find augmenting path that is the shortest path measured in number of nodes rather than weight between src and target
+// in practice converges to a solution quickly
 int edmondsKarp(int src, int dst, unordered_map<int, unordered_map<int, Edge>>& graph, const int N) {
+    // breadth first search, first pair is node to process and the edge used to get there
+    // followed by the path of these pairs used to get to the current step
     queue<pair<pair<int, int>, vector<pair<int, int>>>> q;
     vector<int> visited(N, 0);
-    vector<pair<int, int>> path;
+    vector<pair<int, int>> path;  // track in order to reduce flow after augmenting path found
     q.emplace(make_pair(make_pair(src, INF), path));
-    int node = -1, flow = 0;
-    bool reachedTarget = false;
+    int node = -1, flow = 0;     // dummy values
+    bool reachedTarget = false;  // used to tell if augmenting path was found, if it wasn't flow doesnt need to be modified
 
     while (!q.empty()) {
         node = q.front().first.first;
@@ -35,10 +39,13 @@ int edmondsKarp(int src, int dst, unordered_map<int, unordered_map<int, Edge>>& 
         } else {
             visited[node] = true;
         }
-        if (node == dst) {
+
+        if (node == dst) {  // SUCCESS
             reachedTarget = true;
             break;
         }
+        /* for each edge connected to this node, enqueue all edges that have a flow greater than zero
+         * where the flow is the lesser of the current flow and the flow of the edge */
         for (auto&& kv : graph[node]) {
             auto edgeId = kv.first;
             auto&& edge = kv.second;
@@ -54,6 +61,9 @@ int edmondsKarp(int src, int dst, unordered_map<int, unordered_map<int, Edge>>& 
     if (!reachedTarget) {
         return 0;
     }
+
+    /* increase the flow of the forward edge and decrease the flow of the residual edge
+     * uses edge id because the graph is not simple and can have multiple edges per node pair */
     for (int i = 0; i < path.size(); ++i) {
         auto from = path[i].first;
         auto edgeId = path[i].second;
@@ -75,6 +85,9 @@ int main() {
     int numEdges, numNodes;
     fin >> numEdges >> numNodes;
     unordered_map<int, unordered_map<int, Edge>> edges;
+
+    /* Important: this is not a simple graph. Can have multiple edges per node pair
+     * wasted a lot of time b/c I didn't realize this */
     for (int i = 0; i < numEdges; ++i) {
         int src, dst, weight;
         fin >> src >> dst >> weight;
@@ -90,30 +103,22 @@ int main() {
         residual.from = dst;
         residual.to = src;
         residual.capacity = 0;
-        residual.isResidual = true;
-        edges[src][i] = forward;
-        edges[dst][i] = residual;
+        residual.isResidual = true;  // useful for debugging
+        edges[src][i] = forward;     // use i as edge id to pair up forward and residual edges
+        edges[dst][i] = residual;    // alternatively could use multimap but it's messier IMO
     }
+
     int maxFlow = 0;
-    int flow = 1;
-    int count = 0;
-    string line = "\n" + string(50, '-') + "\n";
+    int flow = 1;  // to get into while loop
+    // keep adding to max flow until an augmenting path cannot be found
     while (flow > 0) {
         int src = 0;
         int dst = numNodes - 1;
         // find augmenting path
         flow = edmondsKarp(src, dst, edges, numNodes);
         maxFlow += flow;
-        ++count;
     }
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 0; j < numNodes; ++j) {
-            string s = to_string(j) + string(1, ':') + to_string(edges[i][j].remainingCapacity());
-            if (j % 10 == 9) {
-                cout << '\n';
-            }
-        }
-    }
+
     ofstream fout("ditch.out");
     fout << maxFlow << '\n';
     return EXIT_SUCCESS;
